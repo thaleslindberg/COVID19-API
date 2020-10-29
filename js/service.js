ec7a328c02e14127376fb76eb669610a5f8e7081
Thales <thales.lindberg@gmail.com>
2020-10-28 23:49:21 -0300

    changes

--- js/service.js
+++ js/service.js
@@ -1,43 +1,116 @@
-const url = 'https://covid19-brazil-api.now.sh/api/report/v1';
+$(document).ready(function() {
+    $("#state_content").hide();
 
-window.onload = () => getEstados();
+const urlCovidApi = 'https://covid19-brazil-api.now.sh/api/report/v1';
 
-function getEstados(){
+getEstados();
+
+function getEstados(stateName) {
     let request = new XMLHttpRequest();
-    request.open('GET' , url);
+    
+    let url = stateName ? `${urlCovidApi}/${stateName}` : urlCovidApi
+
+    request.open('GET' , urlCovidApi);
     request.responseType= 'json';
     request.send();
 
-    request.onload = function(){
-        let responseData = request.response;
-        formatEstados(responseData.data);
-    }
-}
+    request.onload = function(){     
 
-function formatEstados(estados){
-    for(let i = 0; i < estados.length; i++){
+        let responseData = request.response;  
+        let tamanho=  responseData.data.length;
+
         
-        showEstados(estados[i]);
-    }
+       
+        if (stateName) {
+            for(let i =0; i<tamanho;i++){
+                    if(stateName==responseData.data[i].state){
+                        formatEstado(responseData.data[i]);
 
+
+                }   
+            }
+
+            
+
+        } else {
+            formatEstados(responseData.data);
+        }
+    }
 }
 
-function showEstados(estados){
-    $("#state_table").append("<tr>");
-    let object_keys = Object.keys(estados);
+function formatEstado(state){
+    console.log(state)
+   
+
+    if(state.state==null){
+        $("#state_content").append("Not found State")
+    }
+    else {
+
+        $("#tabela").append("<tr>");
+    let object_keys = Object.keys(state);
 
     for (let key = 0; key < object_keys.length; key++){
         let currentKey = object_keys[key];
-        if (currentKey == "state"){
-            $("#state_table").append("<td>" + `${estados.state}` + "</td>")
+
+        if (currentKey == "state") {
+            $("#tabela").append("<td>" + `${state.state}` + "</td>")
         }
-        else if (currentKey == "cases"){
-            $("#state_table").append("<td>" + `${estados.cases}` + "</td>")
+        else if (currentKey == "cases") {
+            $("#tabela").append("<td>" + `${state.cases}` + "</td>")
         }
-        else if (currentKey == "deaths"){
-            $("#state_table").append("<td>" + `${estados.deaths}` + "</td>")
+        else if (currentKey == "deaths") {
+            $("#tabela").append("<td>" + `${state.deaths}` + "</td>")
+        }
+    }
+    $("#tabela").append("</tr>");
+    }
+
+    $("#tabela").append("<a href='#' target='blank>Back</a>")
+
+}
+
+function formatEstados(estados){
+    for(let i = 0; i < estados.length; i++) {
+        
+
+        let state = {
+            state: estados[i].state,
+            cases: estados[i].cases,
+            deaths: estados[i].deaths
+        }
+        
+        showEstados(state);
+    }
+    
+}
+
+function showEstados(state){
+    $("#state_table").append("<tr>");
+    let object_keys = Object.keys(state);
+
+    for (let key = 0; key < object_keys.length; key++){
+        let currentKey = object_keys[key];
+
+        if (currentKey == "state") {
+            $("#state_table").append("<td>" + `${state.state}` + "</td>")
+        }
+        else if (currentKey == "cases") {
+            $("#state_table").append("<td>" + `${state.cases}` + "</td>")
+        }
+        else if (currentKey == "deaths") {
+            $("#state_table").append("<td>" + `${state.deaths}` + "</td>")
         }
     }
     $("#state_table").append("</tr>");
-
 }
+
+$("#button_search").click(function(){
+    $("#home_content").hide();
+    $("#state_content").show();
+
+    let stateName = $("input").val();
+    getEstados(stateName)
+})
+
+})
